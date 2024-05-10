baba82e6c1f855f2fe26e7add88f6e5300c60e83
Stephan <4.da.crap@gmail.com>
2024-05-10 11:42:48 +0200

    index

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ client.query(`
         )
  `);
 
-// app.use(express.json());
+app.use(express.json());
 // app.use((req, res, next) => {
 //     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:' + frontendPort);
 //     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
